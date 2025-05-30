46b59fed1bee1e31938ff88f097975f5604ac231
Alex Mickelson <alexmickelson96@gmail.com>
2025-05-30 21:39:12 +0000

    working, need to handle thinking

--- client/app/features/AiChatContext.tsx
+++ client/app/features/AiChatContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
 import { useTRPCClient } from "./trpc/trpcClient";
 import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
@@ -32,6 +32,127 @@ export const AiChatProvider = ({
   ]);
   const client = useTRPCClient();
 
+  useEffect(() => {
+    console.log(messages);
+    const lastMessage = messages[messages.length - 1];
+    if (!lastMessage) return;
+
+    if (lastMessage.role !== "user" && lastMessage.role !== "tool") {
+      console.log("not starting stream, last message is not user");
+      return;
+    } else {
+      console.log("starting stream with messages:", messages);
+    }
+
+    async function handleToolCall(
+      chunk: OpenAI.Chat.Completions.ChatCompletionChunk
+    ) {
+      console.log("tool call detected", chunk.choices[0].delta.tool_calls);
+
+      const chosenTool = tools.find(
+        (tool) =>
+          tool.name === chunk.choices[0].delta.tool_calls?.[0].function?.name
+      );
+      if (!chosenTool) {
+        console.error("Tool not found:", chunk.choices[0].delta.tool_calls);
+        return;
+      }
+      const params = chunk.choices[0].delta.tool_calls?.[0].function?.arguments;
+      if (!params) {
+        console.error(
+          "No parameters provided for tool call",
+          chunk.choices[0].delta.tool_calls
+        );
+        return;
+      }
+      try {
+        const result = await chosenTool.fn(params);
+        console.log("Tool result:", result);
+
+        let latestMessage: ChatCompletionMessageParam = {
+          role: "assistant",
+          content: `Tool: ${chosenTool.name}\nResult: ${JSON.stringify(
+            result
+          )}`,
+        };
+        latestMessage.content += `\n\n`;
+
+        setMessages((prev) => {
+          const updatedMessages = [...prev];
+          updatedMessages[updatedMessages.length - 1] = { ...latestMessage };
+          return updatedMessages;
+        });
+
+        // Send the tool result back to the LLM as a tool message
+        const tool_call_id =
+          chunk.choices[0].delta.tool_calls?.[0].id ??
+          chunk.choices[0].delta.tool_calls?.[0].function?.name ??
+          "unknown_tool_call_id";
+        const toolMessage: ChatCompletionMessageParam = {
+          role: "tool",
+          content: JSON.stringify(result),
+          tool_call_id,
+        };
+        setMessages((prev) => [...prev, toolMessage]);
+      } catch (error) {
+        console.error("Error executing tool function:", error);
+      }
+    }
+
+    const handleStream = async () => {
+      const toolsSchema: OpenAI.Chat.Completions.ChatCompletionTool[] =
+        tools.map((tool) => ({
+          type: "function",
+          function: {
+            name: tool.name,
+            description: tool.description,
+            parameters: zodToJsonSchema(tool.paramsSchema, {
+              target: "openAi",
+            }),
+          },
+        }));
+
+      const stream = await client.ai.streamOpenAi.query({
+        messages: messages,
+        tools: toolsSchema,
+      });
+
+      let latestMessage: ChatCompletionMessageParam = {
+        role: "assistant",
+        content: "",
+        name: "assistant",
+      };
+      setMessages((prev) => [...prev, latestMessage]);
+
+      for await (const chunk of stream) {
+        console.log(chunk);
+        // if (isCancelled) {
+        //   console.log("stream cancelled");
+        //   return;
+        // } else
+
+        if (chunk.choices[0].finish_reason) {
+          // console.log("stream finished", chunk.choices[0].finish_reason);
+          return chunk.choices[0].finish_reason;
+        }
+        if (chunk.choices[0].delta.tool_calls?.length) {
+          console.log("tool call chunk", chunk);
+          await handleToolCall(chunk);
+        } else {
+          latestMessage.content += chunk.choices[0].delta.content || "";
+          setMessages((prev) => {
+            const updatedMessages = [...prev];
+            updatedMessages[updatedMessages.length - 1] = latestMessage;
+            return updatedMessages;
+          });
+        }
+      }
+    };
+
+    const reason = handleStream();
+    console.log("Stream finished with reason:", reason);
+  }, [messages]);
+
   const sendMessage = async (input: string) => {
     const userMessage: ChatCompletionMessageParam = {
       role: "user",
@@ -39,47 +160,6 @@ export const AiChatProvider = ({
       name: "user",
     };
     setMessages((prev) => [...prev, userMessage]);
-
-    const toolsSchema: OpenAI.Chat.Completions.ChatCompletionTool[] = tools.map(
-      (tool) => ({
-        type: "function",
-        function: {
-          name: tool.name,
-          description: tool.description,
-          parameters: zodToJsonSchema(tool.paramsSchema, {
-            target: "openAi",
-          }),
-        },
-      })
-    );
-
-    const stream = await client.ai.streamOpenAi.query({
-      messages: [...messages, userMessage],
-      tools: toolsSchema,
-    });
-
-    let latestMessage: ChatCompletionMessageParam = {
-      role: "assistant",
-      content: "",
-      name: "assistant",
-    };
-    setMessages((prev) => [...prev, latestMessage]);
-
-    for await (const chunk of stream) {
-      const isToolCall = (chunk.choices[0].delta.tool_calls?.length ?? -1) > 0;
-
-      if (isToolCall) {
-        await handleToolCall(chunk, tools, setMessages);
-        
-      } else {
-        latestMessage.content += chunk.choices[0].delta.content || "";
-        setMessages((prev) => {
-          const updatedMessages = [...prev];
-          updatedMessages[updatedMessages.length - 1] = latestMessage;
-          return updatedMessages;
-        });
-      }
-    }
   };
 
   return (
@@ -96,60 +176,3 @@ export const useAiChat = () => {
   }
   return context;
 };
-
-async function handleToolCall(
-  chunk: OpenAI.Chat.Completions.ChatCompletionChunk,
-  tools: AiTool[],
-  setMessages: React.Dispatch<
-    React.SetStateAction<ChatCompletionMessageParam[]>
-  >
-) {
-  console.log("tool call detected", chunk.choices[0].delta.tool_calls);
-
-  const chosenTool = tools.find(
-    (tool) =>
-      tool.name === chunk.choices[0].delta.tool_calls?.[0].function?.name
-  );
-  if (!chosenTool) {
-    console.error("Tool not found:", chunk.choices[0].delta.tool_calls);
-    return;
-  }
-  const params = chunk.choices[0].delta.tool_calls?.[0].function?.arguments;
-  if (!params) {
-    console.error(
-      "No parameters provided for tool call",
-      chunk.choices[0].delta.tool_calls
-    );
-    return;
-  }
-  try {
-    const result = await chosenTool.fn(params);
-    console.log("Tool result:", result);
-
-    let latestMessage: ChatCompletionMessageParam = {
-      role: "assistant",
-      content: `Tool: ${chosenTool.name}\nResult: ${JSON.stringify(result)}`,
-    };
-    latestMessage.content += `\n\n`;
-
-    setMessages((prev) => {
-      const updatedMessages = [...prev];
-      updatedMessages[updatedMessages.length - 1] = { ...latestMessage };
-      return updatedMessages;
-    });
-
-    // Send the tool result back to the LLM as a tool message
-    const tool_call_id =
-      chunk.choices[0].delta.tool_calls?.[0].id ??
-      chunk.choices[0].delta.tool_calls?.[0].function?.name ??
-      "unknown_tool_call_id";
-    const toolMessage: ChatCompletionMessageParam = {
-      role: "tool",
-      content: JSON.stringify(result),
-      tool_call_id,
-    };
-    setMessages((prev) => [...prev, toolMessage]);
-  } catch (error) {
-    console.error("Error executing tool function:", error);
-  }
-}
